469f28aed36321973cf707e9154d16f5aea0e167
areyesce <69359136+areyesce@users.noreply.github.com>
2022-10-20 15:07:59 -0700

    Update helperFunctions.py

--- helperFunctions.py
+++ helperFunctions.py
@@ -13,5 +13,36 @@ def generateRandomCard(deck):
 	    deck.remove(card)
 	return deck, card
 	
-	
-	
+def displayHands(h, d, h_score, d_score, stand=None, hide=False):
+    if stand:
+        print("stand == 'Player'")
+        if stand == 'Player':
+            h_cards = 'Player stands with: '
+            for i in range(len(h)): 
+                h_cards += str(h[i]) + ' '
+                h_cards += '= ' + str(h_score) + '\n'
+            print(h_cards)
+        elif stand == 'Dealer':
+            d_cards = 'Dealer stands with: '
+            for i in range(len(d)): 
+                d_cards += str(d[i]) + ' ' 		#might have to be .to_string( ) 
+                d_cards += '= ' + str(d_score) + '\n'
+            print(d_cards)
+        return
+    """ assumption: hide is True only at the start of the game;card revelead doesnt matter """
+    if hide: 
+        print('Dealer has: %s %s = %s' %(str(d[0]),"?", "?")) 
+        print('Player has: %s %s = %s'%(h[0], h[1], h_score)) 
+    else:
+        if d != []:
+            d_cards = 'Dealer has: '
+            for i in range(len(d)): 
+            		d_cards += str(d[i]) + ' ' 
+            d_cards += '= ' + str(d_score) + '\n'
+        if h != []:
+            h_cards = 'Player has: '
+            for i in range(len(h)):
+                h_cards += str(h[i]) + ' '
+            h_cards += '= ' + str(h_score) + '\n'
+        if d != [ ]: print(d_cards)
+        if h != [ ]: print(h_cards)
