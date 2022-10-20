1bc0b07f618e9cb5b27f4c2636d8071869f69a1b
areyesce <69359136+areyesce@users.noreply.github.com>
2022-10-20 15:08:20 -0700

    Update startGame.py

--- startGame.py
+++ startGame.py
@@ -1,57 +1,7 @@
 import random
+from helperFunctions import *
 
 # LAST LEFT OFF TRYING TO SEE WHY INPUT 'S' DOES NOT FUNCTION
-def createDeck():
-    deck = []
-    for _ in range(4):
-        deck.extend([x for x in range(2,11)])
-        deck.extend(['J', 'Q', 'K', 'A'])
-    return deck
-
-def generateRandomCard(deck):
-# 	print("DECK!!!!: ",deck)
-# 	print("TYPE: ",type(deck))
-	card = random.choice(deck)
-# 	print("RANDOM CARD:",card)
-	
-	if card in deck:
-	    deck.remove(card)
-# 	print("NEW DECK!!!!: ",deck) 
-	return deck, card
-
-def displayHands(h, d, h_score, d_score, stand=None, hide=False):
-    if stand:
-        print("stand == 'Player'")
-        if stand == 'Player':
-            h_cards = 'Player stands with: '
-            for i in range(len(h)): 
-                h_cards += str(h[i]) + ' '
-                h_cards += '= ' + str(h_score) + '\n'
-            print(h_cards)
-        elif stand == 'Dealer':
-            d_cards = 'Dealer stands with: '
-            for i in range(len(d)): 
-                d_cards += str(d[i]) + ' ' 		#might have to be .to_string( ) 
-                d_cards += '= ' + str(d_score) + '\n'
-            print(d_cards)
-        return
-    """ assumption: hide is True only at the start of the game;card revelead doesnt matter """
-    if hide: 
-        print('Dealer has: %s %s = %s' %(str(d[0]),"?", "?")) 
-        print('Player has: %s %s = %s'%(h[0], h[1], h_score)) 
-    else:
-        if d != []:
-            d_cards = 'Dealer has: '
-            for i in range(len(d)): 
-            		d_cards += str(d[i]) + ' ' 
-            d_cards += '= ' + str(d_score) + '\n'
-        if h != []:
-            h_cards = 'Player has: '
-            for i in range(len(h)):
-                h_cards += str(h[i]) + ' '
-            h_cards += '= ' + str(h_score) + '\n'
-        if d != [ ]: print(d_cards)
-        if h != [ ]: print(h_cards)
 
 def scoreHand(hand):
     number_cards = {2, 3, 4, 5, 6, 7, 8, 9, 10}
@@ -99,7 +49,7 @@ def startGame(root=None):
     	        deck, card = generateRandomCard(deck)
     	        hand.append(card)
     human_score, dealer_score = scoreHand(human_hand), scoreHand(dealer_hand)
-    
+
     """ 2. Display initial hands (hiding dealer"s second card and score) """
     dealer_card_hidden = True
     if dealer_card_hidden:
