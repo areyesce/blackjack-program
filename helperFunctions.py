b9bba1e8cd8ef2b6c8efdb869d71301c3f168570
areyesce <69359136+areyesce@users.noreply.github.com>
2022-10-20 15:05:18 -0700

    Update helperFunctions.py

--- helperFunctions.py
+++ helperFunctions.py
@@ -1,6 +1,17 @@
+import random
 def createDeck():
     deck = []
     for _ in range(4):
         deck.extend([x for x in range(2,11)])
         deck.extend(['J', 'Q', 'K', 'A'])
     return deck
+    
+def generateRandomCard(deck):
+	card = random.choice(deck)
+	
+	if card in deck:
+	    deck.remove(card)
+	return deck, card
+	
+	
+	
